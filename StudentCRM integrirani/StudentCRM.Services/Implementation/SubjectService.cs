35fb358dc5828fc393f775970ed6336660905134
Коваческа Михаела <mihaela.kovacheska@students.finki.ukim.mk>
2023-01-30 01:40:56 +0100

    Added SubjectService and ISubjectService

---
+++ StudentCRM integrirani/StudentCRM.Services/Implementation/SubjectService.cs
@@ -0,0 +1,47 @@
+﻿using StudentCRM.Domain.DomainModels;
+using StudentCRM.Repository.Interface;
+using StudentCRM.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentCRM.Services.Implementation
+{
+    public class SubjectService : ISubjectService
+    {
+        private readonly IRepository<Subject> subjectRepository;
+        
+        public SubjectService(IRepository<Subject> subjectRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+
+        public List<Subject> ListAll()
+        {
+            return this.subjectRepository.GetAll().ToList();
+        }
+
+        public Subject FindById(Guid? id)
+        {
+            return this.subjectRepository.Get(id);
+        }
+
+        public void CreateNewSubject(Subject s)
+        {
+            this.subjectRepository.Insert(s);
+        }
+        
+        public void UpdateExistingSubject(Subject s)
+        {
+            this.subjectRepository.Update(s);
+        }
+
+        public void DeleteSubject(Guid id)
+        {
+            var subject = this.FindById(id);
+            this.subjectRepository.Delete(subject);
+        }
+
+    }
+}
---
+++ StudentCRM integrirani/StudentCRM.Services/Interface/ISubjectService.cs
@@ -0,0 +1,18 @@
+﻿using StudentCRM.Domain.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentCRM.Services.Interface
+{
+    public interface ISubjectService
+    {
+        List<Subject> ListAll();
+        Subject FindById(Guid? id);
+        void CreateNewSubject(Subject s);
+        void UpdateExistingSubject(Guid id, String code, String subjectName);
+        void DeleteSubject(Guid id);
+
+        
+    }
+}
