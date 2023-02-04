5a1050d07d58e11cf96d8901322c5f3b89ff4afd
AnaPeshevska <ana.peshevska.1@gmail.com>
2023-02-04 22:47:17 +0100

    Added UserRepository interface and implementation

---
+++ StudentCRM integrirani/StudentCRM.Repository/Implementation/UserRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentCRM.Domain.Identity;
+using StudentCRM.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentCRM.Repository.Implementation
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext context;
+        private DbSet<ProfessorUser> entities;
+        string errorMessage = string.Empty;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+            entities = context.Set<ProfessorUser>();
+        }
+
+        public IEnumerable<ProfessorUser> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public ProfessorUser Get(int id)
+        {
+            return entities.SingleOrDefault(s => s.Id == id);
+        }
+
+        public void Insert(ProfessorUser entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(ProfessorUser entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Update(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(ProfessorUser entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ StudentCRM integrirani/StudentCRM.Repository/Interface/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using StudentCRM.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentCRM.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        IEnumerable<ProfessorUser> GetAll();
+        ProfessorUser Get(int id);
+        void Insert(ProfessorUser entity);
+        void Update(ProfessorUser entity);
+        void Delete(ProfessorUser entity);
+    }
+}
